6f829322103b62123520ba1d58281a2b8f8242cb
Misty Shaine Niones <125946850+msn02@users.noreply.github.com>
2024-05-02 21:45:03 +0800

    initial file upload
    landing page, and other resources (css, javascript, and images)

---
+++ landing_page.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Ukay Tamis!</title>
+    <?php include 'head_resource.php';?>
+    <link rel="stylesheet" href="css/landing.css">
+</head>
+<body class="gray_bg">
+    <!-- navigation bar -->
+    <?php include 'nav_bar.php'?>
+
+    <div class="container-fluid p-0 gradient_pink">
+        <!-- title and intro -->
+        <div class="container">
+            <div class="row center_align pt-3">
+                <div class="col-sm-8 px-4 pt-3">
+                    <h1 class="bold_header">Thrifted Fashion,<br>Curated for You!</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis auctor elit sed. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit.</p>
+                    <p>Id velit ut tortor pretium viverra suspendisse potenti.</p>
+                </div>
+                <div class="col-sm-12 p-0 pink_btn pb-3">
+                    <button class="btn btn-primary border-0 px-4 shadow rounded-1" type="button">SUBSCRIBE NOW</button>
+                </div>
+            </div>
+        </div>
+        <!-- how UKAY TAMIS works -->
+        <div class="container-fluid mt-3 gray_bg">
+            <div class="container py-3 how_app_work">
+                <h4 class="bold_header pink_highlight2 pt-2">How <strong>Ukay Tamis</strong> Works</h4>
+                <div class="row center_align pt-3 px-5 gx-5 how_app_work">
+                    <div class="col-sm-3">
+                        <div class="card ratio ratio-1x1 mb-3 overflow-hidden">
+                            <img src="resources/1.jpg" class="img-fluid" alt="Subscribe">
+                        </div>
+                        <h5><span class="pink_highlight title1 m-0">Subscribe</span></h5>
+                        <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="card ratio ratio-1x1 mb-3 overflow-hidden">
+                            <img src="resources/2.jpg" class="img-fluid" alt="Subscribe">
+                        </div>
+                        <h5><span class="pink_highlight title1 m-0">We Curate</span></h5>
+                        <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="card ratio ratio-1x1 mb-3">
+                            <img src="resources/3.jpg" class="img-fluid" alt="Subscribe">
+                        </div>
+                        <h5><span class="pink_highlight title1 m-0">Try it on!</span></h5>
+                        <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- mystery box -->
+    <div class="container-fluid m-0 p-0 gradient_green">
+        <div class="container">
+            <div class="row center_align pt-3">
+                <div class="col-sm-8 px-4 pt-3">
+                    <h3 class="bold_header">Rare haul with our MYSTERY BOX!</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis auctor elit sed. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit.</p>
+                    <p>Id velit ut tortor pretium viverra suspendisse potenti.</p>
+                </div>
+                <div class="col-sm-12 p-0 pink_btn pb-3">
+                    <button class="btn btn-primary border-0 px-4 shadow rounded-1" type="button">CHOOSE YOUR PLAN</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid m-0 p-0">
+        <div class="container-fluid center_align px-4 pt-3 yellow_bg">
+            <div class="container black_text mb-2">
+                <h5>What our <span class="bold_header">Subscribers</span> Say</h5>
+            </div>
+        </div>
+
+        <!-- customer reviews -->
+        <div class="container-fluid gradient_yellow py-4">
+            <div class="container p-0 text-right">
+                <div class="container row g-4 m-0 p-0">
+                    <!-- card -->
+                    <div class="col-sm-4 mb-3 mt-0 mx-0">
+                        <div class="card border-0 p-3">
+                            <div class="review_content">
+                                <div class="row">
+                                    <div class="col star_icon mb-2">
+                                        <i class="bi bi-star-fill"></i>
+                                        <i class="bi bi-star-fill"></i>
+                                        <i class="bi bi-star-fill"></i>
+                                        <i class="bi bi-star-fill"></i>
+                                        <i class="bi bi-star-fill"></i>
+                                    </div>
+                                </div>
+                                <h6 class="bold_header">Worth the price!</h6>
+                                <p class="mb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis auctor elit sed. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit.</p>
+                                <span class="bold_header review_name">JUAN DELA CRUZ</span>
+                                <div class="row mt-2 justify-content-start">
+                                    <div class="col-2"><div class="card border-0 gray_sq overflow-hidden">
+                                        <img src="resources/2.jpg" class="img-fluid" alt="Subscribe">
+                                    </div></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 mb-3 mt-0 mx-0">
+                        <div class="card border-0 p-3">
+                            <div class="review_content">
+                                <div class="row">
+                                    <div class="col star_icon mb-2">
+                                        <i class="bi bi-star-fill"></i>
+                                        <i class="bi bi-star-fill"></i>
+                                        <i class="bi bi-star-fill"></i>
+                                        <i class="bi bi-star-fill"></i>
+                                        <i class="bi bi-star-fill"></i>
+                                    </div>
+                                </div>
+                                <h6 class="bold_header">True to size</h6>
+                                <p class="mb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis auctor elit sed. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit.</p>
+                                <span class="bold_header review_name">JUAN DELA CRUZ</span>
+                                <div class="row mt-2 justify-content-start">
+                                    <div class="col-2"><div class="card border-0 gray_sq overflow-hidden">
+                                        <img src="resources/2.jpg" class="img-fluid" alt="Subscribe">
+                                    </div></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-4 mb-3 mt-0 mx-0">
+                        <div class="card border-0 p-3">
+                            <div class="review_content">
+                                <div class="row">
+                                    <div class="col star_icon mb-2">
+                                        <i class="bi bi-star-fill"></i>
+                                        <i class="bi bi-star-fill"></i>
+                                        <i class="bi bi-star-fill"></i>
+                                        <i class="bi bi-star-fill"></i>
+                                        <i class="bi bi-star-fill"></i>
+                                    </div>
+                                </div>
+                                <h6 class="bold_header">Delivered on time</h6>
+                                <p class="mb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis auctor elit sed. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit.</p>
+                                <span class="bold_header review_name">JUAN DELA CRUZ</span>
+                                <div class="row mt-2 justify-content-start">
+                                    <div class="col-2"><div class="card border-0 gray_sq overflow-hidden">
+                                        <img src="resources/2.jpg" class="img-fluid" alt="Subscribe">
+                                    </div></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- TO DO: footer -->
+</body>
+</html>
---
+++ nav_bar.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg dark_gray_bg sticky-top px-3">
+    <div class="container">
+        <a class="navbar-brand page_title" href="landing_page.php"><span class="pink_highlight">UKAY</span> <span class="white_text">TAMIS</span></a>
+        <!-- toggler -->
+        <button class="navbar-toggler btn shadow-none border-0 justify-content-end m-0 p-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarLink" aria-controls="navbarLink" aria-expanded="false" aria-label="Toggler">
+            <div class="burger_icon justify-content-end">
+                <i class="bi bi-list"></i>
+            </div>
+        </button>
+        <!-- navbar contents -->
+        <div class="collapse navbar-collapse justify-content-end dark_gray_bg" id="navbarLink">
+            <div class="m-0 p-0">
+                <ul class="navText navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="landing_page.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Catalogue</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                    <!-- shopping cart -->
+                    <li class="nav-item cart_icon">
+                        <a class="nav-link position-relative" href="#">
+                            <i class="bi bi-cart-fill"></i>
+                        </a>
+                        <!-- TO DO: Add a badge -->
+                    </li>
+                    <!-- profile -->
+                    <li class="nav-item cart_icon">
+                        <a class="nav-link" href="#"><i class="bi bi-person-circle"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
