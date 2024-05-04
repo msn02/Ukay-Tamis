854254f19fd95d60e42e6f17a40f1717be837d6f
Daniela Cantillo <danielamcantillo@gmail.com>
2024-05-04 19:25:11 +0800

    feat: add connection.php

--- head_resource.php
+++ head_resource.php
@@ -1,3 +1,5 @@
+<?php include('server/connection.php'); ?>
+
 <!-- CSS and JavaScript Resources -->
 <link rel="stylesheet" href="css/main_style.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
