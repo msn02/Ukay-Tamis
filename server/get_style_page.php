2ef3144caf68c326b6e1fb1af36420a817dee8cf
Daniela Cantillo <danielamcantillo@gmail.com>
2024-05-04 18:38:55 +0800

    feat: display contents on page and get related items

---
+++ server/get_style_page.php
@@ -0,0 +1,24 @@
+<?php
+    // Include the connection.php file
+    include('connection.php');
+    
+    if (isset($_GET['style_id'])) {
+        $style_id = $_GET['style_id'];
+
+        // Prepare the SQL query
+        $stmt = $conn -> prepare ("SELECT * FROM style_box_details WHERE style_id = ?");
+
+        // Bind the parameters
+        $stmt -> bind_param("i", $style_id);
+
+        // Execute the query
+        $stmt -> execute();
+       
+        // Get the result
+        $style = $stmt -> get_result();
+
+
+    } else {
+        header('location: catalogue_page.php');
+    }
+?>
