f55e27eb5c23f72af7a9bfe8029dbb8d34f2ad3d
Misty Shaine Niones <125946850+msn02@users.noreply.github.com>
2024-05-05 20:18:31 +0800

    refactor: added link for checkout btn

--- view_cart.php
+++ view_cart.php
@@ -79,7 +79,7 @@
                         </div>
                     </div>
                     <div class="pink_btn2 pb-3">
-                        <button class="btn btn-dark border-0 rounded-1 w-100">CHECKOUT</button>
+                        <a href="checkout_page.php"><button class="btn btn-dark border-0 rounded-1 w-100">CHECKOUT</button></a>
                     </div>
                 </div>
             </div>
