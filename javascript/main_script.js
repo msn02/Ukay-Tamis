7d9aede0ef2bc7c2f9ff8f1400110dcdc3e46355
Misty Shaine Niones <125946850+msn02@users.noreply.github.com>
2024-05-05 14:07:57 +0800

    refactor: added comments

--- javascript/main_script.js
+++ javascript/main_script.js
@@ -1,9 +1,19 @@
+// navigation bar
 document.querySelectorAll('.nav-link').forEach(link=>{
 	if (link.href===window.location.href){
 		link.setAttribute('aria-current','page');
 	}
 });
 
+// back button
 document.getElementById('go_back').addEventListener('click', function() {
 	window.history.back();
+});
+
+// sign up page
+const checkbox = document.getElementById('checkbox_agree');
+const signup = document.getElementById('signup_btn');
+
+checkbox.addEventListener('change', function() {
+	signup.disabled = !checkbox.checked;
 });
