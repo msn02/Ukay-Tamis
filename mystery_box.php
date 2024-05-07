9fe3d919260bbc01fc53b96f06329afff0aabf03
Misty Shaine Niones <125946850+msn02@users.noreply.github.com>
2024-05-07 22:38:46 +0800

    style: removed picture sets, modified layout

--- mystery_box.php
+++ mystery_box.php
@@ -25,121 +25,155 @@
                 </div>
 
                 <div class="col-10 mt-3 p-0">
-                    <div class="card rounded-1 border-0 shadow gray_bg overflow-hidden">
+                    <div class="card rounded-2 border-0 shadow gray_bg overflow-hidden">
                         <div class="header_style text-center p-3">
                             <h1 class="p-0 m-0 bold_header2">Haul <span class="green_highlight">RARE</span> items today!</h1>
                         </div>
                         
-                        <div class="row m-4 p-0 d-flex justify-content-between">
+                        <div class="m-4 p-3 center_align">
                             <!-- how the feature works -->
-                            <div class="col-sm-4 p-4 border-o shadow-sm rounded-2 yellow_bg">
-                                <h5 class="bold_header2 mb-4 text-center ">How MYSTERY boxes work</h5>
+                            <div class="row p-4 border-o center_align rounded-2 yellow_bg">
+                                <h5 class="bold_header2 mb-4 text-center pink_highlight2">How MYSTERY boxes work</h5>
                                 <!-- 1 -->
-                                <div class="card border-0 overflow-hidden rounded-2 mb-2">
-                                    <div class="card-body">
-                                        <div class="card-title row p-1">
-                                            <div class="col-sm-1 center_align px-3 py-0">
-                                                <h4 class="bold_header">1</h4>
-                                            </div>
-                                            <div class="col-sm-10 card_content pl-3 d-flex align-items-center">
-                                                <div class="m-0 p-0">
-                                                    <h6 class="m-0 p-0 bold_header">Add to Cart</h6>
-                                                    <p class="m-0 p-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <div class="col-sm-4">
+                                    <div class="card border-0 overflow-hidden rounded-2 mb-2">
+                                        <div class="card-body">
+                                            <div class="card-title row p-1">
+                                                <div class="col-sm-1 center_align px-3 py-0">
+                                                    <h4 class="bold_header">1</h4>
+                                                </div>
+                                                <div class="col-sm-10 card_content pl-3 d-flex align-items-center">
+                                                    <div class="m-0 p-0">
+                                                        <h6 class="m-0 p-0 bold_header">Add to Cart</h6>
+                                                        <p class="m-0 p-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                                 <!-- 2 -->
-                                <div class="card border-0 overflow-hidden rounded-2 mb-2">
-                                    <div class="card-body">
-                                        <div class="card-title row p-1">
-                                            <div class="col-sm-1 center_align px-3 py-0">
-                                                <h4 class="bold_header">2</h4>
-                                            </div>
-                                            <div class="col-sm-10 card_content pl-3 d-flex align-items-center">
-                                                <div class="m-0 p-0">
-                                                    <h6 class="m-0 p-0 bold_header">Insert Title</h6>
-                                                    <p class="m-0 p-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <div class="col-sm-4">
+                                    <div class="card border-0 overflow-hidden rounded-2 mb-2">
+                                        <div class="card-body">
+                                            <div class="card-title row p-1">
+                                                <div class="col-sm-1 center_align px-3 py-0">
+                                                    <h4 class="bold_header">2</h4>
+                                                </div>
+                                                <div class="col-sm-10 card_content pl-3 d-flex align-items-center">
+                                                    <div class="m-0 p-0">
+                                                        <h6 class="m-0 p-0 bold_header">Insert Title</h6>
+                                                        <p class="m-0 p-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                                 <!-- 3 -->
-                                <div class="card border-0 overflow-hidden rounded-2 mb-2">
-                                    <div class="card-body">
-                                        <div class="card-title row p-1">
-                                            <div class="col-sm-1 center_align px-3 py-0">
-                                                <h4 class="bold_header">3</h4>
-                                            </div>
-                                            <div class="col-sm-10 card_content pl-3 d-flex align-items-center">
-                                                <div class="m-0 p-0">
-                                                    <h6 class="m-0 p-0 bold_header">Insert Title</h6>
-                                                    <p class="m-0 p-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <div class="col-sm-4">
+                                    <div class="card border-0 overflow-hidden rounded-2 mb-2">
+                                        <div class="card-body">
+                                            <div class="card-title row p-1">
+                                                <div class="col-sm-1 center_align px-3 py-0">
+                                                    <h4 class="bold_header">3</h4>
+                                                </div>
+                                                <div class="col-sm-10 card_content pl-3 d-flex align-items-center">
+                                                    <div class="m-0 p-0">
+                                                        <h6 class="m-0 p-0 bold_header">Insert Title</h6>
+                                                        <p class="m-0 p-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                
-                            </div>
-
-                            <div class="col-sm-8 px-5 py-3">
-                                <h4 class="text-center bold_header2 pink_highlight2 mb-3">Random items, Random STYLES</h4>
-                                <!-- image cards -->
-                                <div class="row g-3 mb-3">
-                                    <div class="col-sm-3">
-                                        <div class="card ratio ratio-1x1 shadow-sm border-0 overflow-hidden rounded-2">
-                                            <img src="resources/gothic_lolita.jpg" alt="">
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-3">
-                                        <div class="card ratio ratio-1x1 shadow-sm border-0 overflow-hidden rounded-2">
-                                            <img src="resources/cottagecore.jpg" alt="">
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-3">
-                                        <div class="card ratio ratio-1x1 shadow-sm border-0 overflow-hidden rounded-2">
-                                            <img src="resources/streetwear.jpg" alt="">
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-3">
-                                        <div class="card ratio ratio-1x1 shadow-sm border-0 overflow-hidden rounded-2">
-                                            <img src="resources/coquette.jpg" alt="">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row g-3">
-                                    <div class="col-sm-3">
-                                        <div class="card ratio ratio-1x1 shadow-sm border-0 overflow-hidden rounded-2">
-                                            <img src="resources/gothic_lolita.jpg" alt="">
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-3">
-                                        <div class="card ratio ratio-1x1 shadow-sm border-0 overflow-hidden rounded-2">
-                                            <img src="resources/cottagecore.jpg" alt="">
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-3">
-                                        <div class="card ratio ratio-1x1 shadow-sm border-0 overflow-hidden rounded-2">
-                                            <img src="resources/streetwear.jpg" alt="">
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-3">
-                                        <div class="card ratio ratio-1x1 shadow-sm border-0 overflow-hidden rounded-2">
-                                            <img src="resources/coquette.jpg" alt="">
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="row m-3 p-2">
+                                <div class="row mt-3 p-0">
                                     <!-- add to cart + checkout btn -->
                                     <div class="center_align pink_btn2 pb-2">
-                                        <a href="#"><button class="btn btn-dark border-0 rounded-1 px-3"><i class="bi bi-cart-plus me-2"></i>ADD TO CART</button></a>
+                                        <a href="#"><button class="btn btn-dark border-0 rounded-1 px-3 shadow"><i class="bi bi-cart-plus me-2"></i>ADD TO CART</button></a>
                                     </div>
-                                    <div class="center_align gray_btn pb-3">
-                                        <a href="checkout_page.php"><button class="btn btn-secondary border-0 rounded-1 px-3">CHECKOUT</button></a>
+                                </div> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-10 mt-3 p-0">
+                    <div class="card rounded-2 border-0 shadow-sm gray_bg overflow-hidden">
+                        <!-- customer reviews (static) -->
+                        <div class="m-4 p-3 center_align">
+                            <div class="row p-4 border-2 green_border rounded-2">
+                                <h4 class="text-center bold_header green_highlight2 mb-4">See what others received from our <span class="green_highlight">MYSTERY BOXES</span> !</h4>
+                                <div class="col-sm-4 mb-3 mt-0 mx-0">
+                                    <div class="card border-1 p-3">
+                                        <div class="review_content">
+                                            <div class="row">
+                                                <div class="col star_icon mb-2">
+                                                    <i class="bi bi-star-fill"></i>
+                                                    <i class="bi bi-star-fill"></i>
+                                                    <i class="bi bi-star-fill"></i>
+                                                    <i class="bi bi-star-fill"></i>
+                                                    <i class="bi bi-star-fill"></i>
+                                                </div>
+                                            </div>
+                                            <h6 class="bold_header">Items gets better and BETTER!</h6>
+                                            <p class="mb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis auctor elit sed. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit.</p>
+                                            <span class="bold_header review_name">JUAN DELA CRUZ</span>
+                                            <div class="row mt-2 justify-content-start">
+                                                <div class="col-4"><div class="card border-0 gray_sq overflow-hidden">
+                                                    <img src="resources/2.jpg" class="img-fluid" alt="Subscribe">
+                                                </div></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-4 mb-3 mt-0 mx-0">
+                                    <div class="card border-1 p-3">
+                                        <div class="review_content">
+                                            <div class="row">
+                                                <div class="col star_icon mb-2">
+                                                    <i class="bi bi-star-fill"></i>
+                                                    <i class="bi bi-star-fill"></i>
+                                                    <i class="bi bi-star-fill"></i>
+                                                    <i class="bi bi-star-fill"></i>
+                                                    <i class="bi bi-star-fill"></i>
+                                                </div>
+                                            </div>
+                                            <h6 class="bold_header">New STYLES to try</h6>
+                                            <p class="mb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis auctor elit sed. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit.</p>
+                                            <span class="bold_header review_name">JUAN DELA CRUZ</span>
+                                            <div class="row mt-2 justify-content-start">
+                                                <div class="col-4"><div class="card border-0 gray_sq overflow-hidden">
+                                                    <img src="resources/2.jpg" class="img-fluid" alt="Subscribe">
+                                                </div></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-4 mb-3 mt-0 mx-0">
+                                    <div class="card border-1 p-3">
+                                        <div class="review_content">
+                                            <div class="row">
+                                                <div class="col star_icon mb-2">
+                                                    <i class="bi bi-star-fill"></i>
+                                                    <i class="bi bi-star-fill"></i>
+                                                    <i class="bi bi-star-fill"></i>
+                                                    <i class="bi bi-star-fill"></i>
+                                                    <i class="bi bi-star-fill"></i>
+                                                </div>
+                                            </div>
+                                            <h6 class="bold_header">LOVE THE SKIRTS</h6>
+                                            <p class="mb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis auctor elit sed. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit.</p>
+                                            <span class="bold_header review_name">JUAN DELA CRUZ</span>
+                                            <div class="row mt-2 justify-content-start">
+                                                <div class="col-4"><div class="card border-0 gray_sq overflow-hidden">
+                                                    <img src="resources/2.jpg" class="img-fluid" alt="Subscribe">
+                                                </div></div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
