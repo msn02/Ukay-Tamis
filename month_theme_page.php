cef957534d7588da7fadcb16fca61a4d6631cb4c
Misty Shaine Niones <125946850+msn02@users.noreply.github.com>
2024-05-09 01:09:09 +0800

    style: removed subscription rates

--- month_theme_page.php
+++ month_theme_page.php
@@ -7,7 +7,7 @@
     <?php include 'head_resource.php';?>
     <link rel="stylesheet" href="css/month_theme.css">
 </head>
-<body class="gray_bg">
+<body class="gray_bg2">
     <!-- navigation bar -->
     <?php include 'nav_bar.php'?>
 
@@ -38,146 +38,90 @@
             </div>
         </div>
     </div>
-    <!-- subscription options -->
-    <div class="container-fluid px-3 pt-1">
-        <div class="container px-5 py-3 mt-0">
-            <div class="row m-2 p-0 d-flex justify-content-center">
-                <div class="col-sm-8 gray_bg rounded-3 p-0 m-2 text-center overflow-hidden">
-                    <div class="row py-3 px-4 d-flex justify-content-evenly">
-                    </div>
-                    <!-- subscription plan -->
-                    <div class="header_style2 px-4 pt-3 pb-2 m-0 ">
-                        <h5 class="bold_header">CHOOSE YOUR <span class="green_highlight">SUBSCRIPTION PLAN</span></h5>
 
-                    </div>
-                    <div class="row py-3 px-4 d-flex justify-content-evenly">
-                        <!-- prices -->
-                        <div class="col-sm-4 img_style p-2">
-                            <div class="card rounded-1 border-1 m-0 p-0 overflow-hidden">
-                                <div class="card-body price_card mb-0">
-                                    <h2 class="pink_highlight2 bold_header">PHP 399/<sup>mo</sup></h2>
-                                    <h6 class="card-title month_title mt-2">Includes..</h6>
-                                </div>
-                                <div class="card-body pink_btn2 mt-0 p-2">
-                                    <div class="col-sm-12 p-0 pink_btn pb-3 mb-3 btn_text">
-                                        <a class="text-decoration-none"><button class="btn btn-dark border-0 px-4 shadow rounded-1" type="button">Add to Card</button></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-4 img_style p-2">
-                            <div class="card rounded-1 border-1 m-0 p-0 overflow-hidden">
-                                <div class="card-body price_card mb-0">
-                                    <h2 class="pink_highlight2 bold_header">PHP 699/<sup>mo</sup></h2>
-                                    <h6 class="card-title month_title mt-2">Includes..</h6>
-                                </div>
-                                <div class="card-body pink_btn2 mt-0 p-2">
-                                    <div class="col-sm-12 p-0 pink_btn pb-3 mb-3 btn_text">
-                                        <a class="text-decoration-none"><button class="btn btn-dark border-0 px-4 shadow rounded-1" type="button">Add to Card</button></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-4 img_style p-2">
-                            <div class="card shadow-sm price_badge rounded-1 border-0 m-0 p-0 overflow-hidden">
-                                <div class="card-body price_card mb-0">
-                                    <h2 class="pink_highlight2 bold_header">PHP 999/<sup>mo</sup></h2>
-                                    <h6 class="card-title month_title mt-2">Includes..</h6>
-                                </div>
-                                <div class="card-body pink_btn2 mt-0 p-2">
-                                    <div class="col-sm-12 p-0 pink_btn pb-3 mb-3 btn_text">
-                                        <a class="text-decoration-none"><button class="btn btn-dark border-0 px-4 shadow rounded-1" type="button">Add to Card</button></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- customer reviews -->
     <div class="container-fluid m-0 p-0">
-        <div class="container-fluid center_align center_text px-4 pt-3 yellow_bg">
-            <div class="container black_text mb-2">
-                <h5>What our <span class="bold_header">Subscribers</span> Say</h5>
-            </div>
-        </div>
-
-        <div class="container-fluid gradient_yellow pb-4">
-            <div class="container p-0 text-right">
-                <div class="container row g-4 m-0 p-5">
-                    <!-- card -->
-                    <div class="col-sm-4 mb-3 mt-0 mx-0">
-                        <div class="card border-0 p-3">
-                            <div class="review_content">
-                                <div class="row">
-                                    <div class="col star_icon mb-2">
-                                        <i class="bi bi-star-fill"></i>
-                                        <i class="bi bi-star-fill"></i>
-                                        <i class="bi bi-star-fill"></i>
-                                        <i class="bi bi-star-fill"></i>
-                                        <i class="bi bi-star-fill"></i>
+        <!-- customer reviews -->
+        <div class="container-fluid pb-2 pt-2 px-4 d-flex justify-content-center">
+            <div class="container mt-0">
+                <div class="p-4 m-5 rounded-2 card border-0 rounded-2">
+                    <div class="green_border row p-4 m-3 rounded-2">
+                    <h4 class="text-center bold_header mb-4">What our Subscribers Say</h4>
+                        <!-- card -->
+                        <div class="col-sm-4 mb-3 mt-0 mx-0">
+                            <div class="card border-0 p-3">
+                                <div class="review_content">
+                                    <div class="row">
+                                        <div class="col star_icon mb-2">
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                        </div>
                                     </div>
-                                </div>
-                                <h6 class="bold_header">Worth the price!</h6>
-                                <p class="mb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis auctor elit sed. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit.</p>
-                                <span class="bold_header review_name">JUAN DELA CRUZ</span>
-                                <div class="row mt-2 justify-content-start">
-                                    <div class="col-2"><div class="card border-0 gray_sq overflow-hidden">
-                                        <img src="resources/2.jpg" class="img-fluid" alt="Subscribe">
-                                    </div></div>
+                                    <h6 class="bold_header">Worth the price!</h6>
+                                    <p class="mb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis auctor elit sed. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit.</p>
+                                    <span class="bold_header review_name">JUAN DELA CRUZ</span>
+                                    <div class="row mt-2 justify-content-start">
+                                        <div class="col-2"><div class="card border-0 gray_sq overflow-hidden">
+                                            <img src="resources/2.jpg" class="img-fluid" alt="Subscribe">
+                                        </div></div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div class="col-sm-4 mb-3 mt-0 mx-0">
-                        <div class="card border-0 p-3">
-                            <div class="review_content">
-                                <div class="row">
-                                    <div class="col star_icon mb-2">
-                                        <i class="bi bi-star-fill"></i>
-                                        <i class="bi bi-star-fill"></i>
-                                        <i class="bi bi-star-fill"></i>
-                                        <i class="bi bi-star-fill"></i>
-                                        <i class="bi bi-star-fill"></i>
+                        <div class="col-sm-4 mb-3 mt-0 mx-0">
+                            <div class="card border-0 p-3">
+                                <div class="review_content">
+                                    <div class="row">
+                                        <div class="col star_icon mb-2">
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                        </div>
                                     </div>
-                                </div>
-                                <h6 class="bold_header">My go-to shop for clothes!</h6>
-                                <p class="mb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis auctor elit sed. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit.</p>
-                                <span class="bold_header review_name">JUAN DELA CRUZ</span>
-                                <div class="row mt-2 justify-content-start">
-                                    <div class="col-2"><div class="card border-0 gray_sq overflow-hidden">
-                                        <img src="resources/2.jpg" class="img-fluid" alt="Subscribe">
-                                    </div></div>
+                                    <h6 class="bold_header">My go-to shop for clothes!</h6>
+                                    <p class="mb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis auctor elit sed. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit.</p>
+                                    <span class="bold_header review_name">JUAN DELA CRUZ</span>
+                                    <div class="row mt-2 justify-content-start">
+                                        <div class="col-2"><div class="card border-0 gray_sq overflow-hidden">
+                                            <img src="resources/2.jpg" class="img-fluid" alt="Subscribe">
+                                        </div></div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div class="col-sm-4 mb-3 mt-0 mx-0">
-                        <div class="card border-0 p-3">
-                            <div class="review_content">
-                                <div class="row">
-                                    <div class="col star_icon mb-2">
-                                        <i class="bi bi-star-fill"></i>
-                                        <i class="bi bi-star-fill"></i>
-                                        <i class="bi bi-star-fill"></i>
-                                        <i class="bi bi-star-fill"></i>
-                                        <i class="bi bi-star-fill"></i>
+                        <div class="col-sm-4 mb-3 mt-0 mx-0">
+                            <div class="card border-0 p-3">
+                                <div class="review_content">
+                                    <div class="row">
+                                        <div class="col star_icon mb-2">
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                            <i class="bi bi-star-fill"></i>
+                                        </div>
+                                    </div>
+                                    <h6 class="bold_header">LOVE THE TOPS!</h6>
+                                    <p class="mb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis auctor elit sed. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit.</p>
+                                    <span class="bold_header review_name">JUAN DELA CRUZ</span>
+                                    <div class="row mt-2 justify-content-start">
+                                        <div class="col-2"><div class="card border-0 gray_sq overflow-hidden">
+                                            <img src="resources/2.jpg" class="img-fluid" alt="Subscribe">
+                                        </div></div>
                                     </div>
                                 </div>
-                                <h6 class="bold_header">LOVE THE TOPS!</h6>
-                                <p class="mb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis auctor elit sed. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit.</p>
-                                <span class="bold_header review_name">JUAN DELA CRUZ</span>
-                                <div class="row mt-2 justify-content-start">
-                                    <div class="col-2"><div class="card border-0 gray_sq overflow-hidden">
-                                        <img src="resources/2.jpg" class="img-fluid" alt="Subscribe">
-                                    </div></div>
-                                </div>
                             </div>
+                        </div>
+
+                        <!-- choose a plan -->
+                        <div class="d-flex justify-content-center p-0 pink_btn pt-2 btn_text">
+                            <a href="subscription_price.php" class="text-decoration-none"><button class="btn btn-dark border-0 px-4 shadow rounded-1" type="button">CHOOSE A PLAN</button></a>
                         </div>
                     </div>
                 </div>
@@ -185,6 +129,7 @@
         </div>
     </div>
 
+    <!-- timer -->
     <div class="container-fluid m-0 p-0 bg-light py-5 bg-dark text-light">
         <div class="container text-center">
             <div class="timer">
