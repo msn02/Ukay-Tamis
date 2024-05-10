9020708f8830ed984d0ade036b4071820feae4a8
Daniela Cantillo <danielamcantillo@gmail.com>
2024-05-10 19:15:06 +0800

    chore: rename the file

---
+++ server/get_related_item_style.php
@@ -0,0 +1,24 @@
+<?php
+    // Include the connection.php file
+    include('connection.php');
+    
+    if (isset($_GET['style_id'])) {
+        $style_id = $_GET['style_id'];
+
+        // Prepare the SQL query
+        $stmt = $conn -> prepare ("SELECT * FROM item INNER JOIN style ON item.style_id = style.style_id WHERE item.style_id = ?");
+
+        // Bind the parameters
+        $stmt -> bind_param("s", $style_id);
+
+        // Execute the query
+        $stmt -> execute();
+       
+        // Get the result
+        $related_items = $stmt -> get_result();
+
+
+    } else {
+        header('location: catalogue_page.php');
+    }
+?>
