ac93d73c1376d917585800eb80ab733bb60628d8
Misty Shaine Niones <125946850+msn02@users.noreply.github.com>
2024-05-10 20:13:36 +0800

    refactor: changed link for logo

--- nav_bar.php
+++ nav_bar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg dark_gray_bg sticky-top">
     <div class="container px-5">
-        <a class="navbar-brand page_title" href="landing_page.php"><span class="pink_highlight">UKAY</span> <span class="white_text">TAMIS</span></a>
+        <a class="navbar-brand page_title" href="index.php"><span class="pink_highlight">UKAY</span> <span class="white_text">TAMIS</span></a>
         <!-- toggler -->
         <button class="navbar-toggler btn shadow-none border-0 justify-content-end m-0 p-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarLink" aria-controls="navbarLink" aria-expanded="false" aria-label="Toggler">
             <div class="burger_icon justify-content-end">
@@ -43,7 +43,6 @@
                             <li><a class="dropdown-item disabled" href="account.php">My Account</a></li>
                             <li><a class="dropdown-item disabled" href="#">Log out</a></li>
                         </ul>
-                        <!-- TO DO: Log in/out, Sign up dropdown options -->
                     </li>
                 </ul>
             </div>
