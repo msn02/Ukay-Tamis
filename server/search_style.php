f3cf54b10896cc51db8e28d32d841e9be1dd5cf1
Daniela Cantillo <danielamcantillo@gmail.com>
2024-05-11 16:41:16 +0800

    refactor: get details for the style box instead of just style to handle transactions

--- server/search_style.php
+++ server/search_style.php
@@ -2,7 +2,7 @@
     if (isset($_GET['search_input'])) {
         $search = $_GET['search_input'];
 
-        $stmt = $conn -> prepare ("SELECT * FROM style WHERE style_id <> 'style-0068' AND style LIKE ?");
+        $stmt = $conn -> prepare ("SELECT * FROM style_box_details WHERE style_id <> 'style-0068' AND style LIKE ?");
     
         $stmt -> bind_param("s", $search);
     
