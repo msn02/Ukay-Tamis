20664930e3cbf9d9e1bbeb81618f81a7031bffd2
Daniela Cantillo <danielamcantillo@gmail.com>
2024-05-13 05:47:27 +0800

    feat: add checkout nav bar

---
+++ checkout_nav_bar.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-expand-lg dark_gray_bg sticky-top">
+    <div class="container px-5">
+        <a class="navbar-brand page_title" href="landing_page.php"><span class="pink_highlight">UKAY</span> <span class="white_text">TAMIS</span></a>
+        <!-- toggler -->
+        <button class="navbar-toggler btn shadow-none border-0 justify-content-end m-0 p-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarLink" aria-controls="navbarLink" aria-expanded="false" aria-label="Toggler">
+            <div class="burger_icon justify-content-end">
+                <i class="bi bi-list"></i>
+            </div>
+        </button>
+    </div>
+</nav>
