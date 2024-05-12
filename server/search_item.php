0a90062b6cbe1de8d9574a097de397ef5f226c8b
Daniela Cantillo <danielamcantillo@gmail.com>
2024-05-13 02:49:49 +0800

    fix: remove sold out item from search result

--- server/search_item.php
+++ server/search_item.php
@@ -3,8 +3,8 @@
         $search = $_GET['search_input'];
         $search = "%".$search."%"; // Add wildcard characters to the search term
 
-        $stmt = $conn -> prepare ("SELECT * FROM item WHERE item_name OR style LIKE ?");
-        $stmt -> bind_param("s", $search);
+        $stmt = $conn -> prepare ("SELECT * FROM item WHERE (item_name LIKE ? OR style LIKE ?) AND transaction_id IS NULL");
+        $stmt -> bind_param("ss", $search, $search);
         $stmt -> execute();
     
         $featured_styles = $stmt -> get_result();
