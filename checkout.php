46dd87d719b4600ed8db95a24657f2a456dd5905
Guirald Escober <guiraldmaloneespaldon.escober@bicol-u.edu.ph>
2024-05-12 22:49:27 +0800

    Revert "Merge branch 'dev-daniela' into guirald-branch"
    This reverts commit 889c17bf7590f2ab907bb09e9714b764f4129448, reversing
    changes made to 3b47df874056ba234fb2a7cc36dbfcc2dee19929.

--- checkout.php
+++ checkout.php
@@ -1,82 +1,22 @@
 <?php
-    // include the connection.php file
+
     include ('server/connection.php');
 
-    // start the session
     session_start();
 
-    // make a condition to lock the checkout page if the user is not logged in
-    if (!isset ($_SESSION['logged_in'])) {
+    if (isset ($_SESSION['logged_in'])) {
+
+        if (isset($_POST['checkout'])) {
+
+        }
+
+    }
+    else {
         header("Location: log_in.php");
         exit();
     }
 
-    // check if user is logged in
-    if (isset ($_SESSION['logged_in'])) {
 
-        // set the total items in the cart
-        $_SESSION['grand_total'] = $_SESSION['total'] + 100;
-
-        // if user clicks the checkout button
-        if (isset($_POST['place_order'])) {
-            // get the payment method
-            
-            if(empty($_POST['payment_method'])) {
-                echo '<script>alert("Please select a payment method")</script>';
-            }
-            
-            $payment_method = $_POST['payment_method'];
-
-            // insert the transaction details to the database
-            $stmt = $conn -> prepare ("INSERT INTO transaction (user_id, total_items, total_price, payment_method) VALUES (?, ?, ?, ?)");
-            
-            $stmt -> bind_param("ssss", $_SESSION['user_id'], $_SESSION['total_items'], $_SESSION['grand_total'], $payment_method);
-            
-            // insert the order items to the order_items table
-            if ($stmt -> execute()) {
-
-                $transaction_id = $conn -> insert_id;
-
-                foreach ($_SESSION['cart'] as $key => $value) {
-                    // If the transaction was inserted successfully, insert the order_product
-                    echo 'type: ' . $value['product_type'];
-
-                    if ($value['product_type'] == 'item') {
-
-                        $stmt = $conn -> prepare ("UPDATE item SET transaction_id = ? WHERE item_id = ? AND item_name = ?");
-                        $stmt -> bind_param("sss", $transaction_id, $value['product_id'], $value['product']);
-                    
-                    } else if ($value['product_type'] == 'style box') {
-                        
-                        $stmt = $conn -> prepare ("INSERT INTO style_box_transaction (transaction_id, style_box_id, style_box_quantity) VALUES (?, ?, ?)");
-                        $stmt -> bind_param("sss", $transaction_id, $value['product_id'], $value['product_quantity']);
-                    }
-                
-                    if ($stmt -> execute()) {
-                        echo '<script>console.log("Order item added successfully")</script>';
-                    } else {
-                        echo '<script>console.log("Failed to add order item")</script>';
-                    }
-                }
-            } else {
-                echo '<script>console.log("Failed to add transaction")</script>';
-            }
-    
-            // clear the cart
-            $_SESSION['cart'] = array();
-            $_SESSION['total_items'] = 0;
-            $_SESSION['total'] = 0;
-            $_SESSION['grand_total'] = 0;
-    
-            // redirect to the checkout success page
-            echo '<script>alert("Order successful")</script>';
-            header("Location: catalogue_page.php");
-        } 
-    }
-    else {
-        header("Location: catalogue_page.php");
-        exit();
-    }
 ?>
 
 
@@ -124,12 +64,12 @@
                             <tr class="product_info align-middle">
                                 <!-- product ordered -->
                                 <th scope="row" class="item_img d-flex justify-content-center">
-                                    <img src="resources/<?php echo $value['product_img_url']; ?>" class="card m-0" alt="item">
+                                    <img src="resources/<?php echo $value['style_img_url']; ?>" class="card m-0" alt="item">
                                 </th>
-                                <td><?php echo $value['product']; ?></td>
-                                <td class="text-center"><?php echo $value['product_price']; ?></td>
-                                <td class="text-center"><?php echo $value['product_quantity']?></td>
-                                <td class="text-center bold_header"><span class="pink_highlight2 "><?php echo 'PHP ' . $value['product_price'] * $value['product_quantity']; ?></span></td>
+                                <td><?php echo $value['style']; ?></td>
+                                <td class="text-center"><?php echo $value['style_price']; ?></td>
+                                <td class="text-center"><?php echo $value['style_quantity']?></td>
+                                <td class="text-center bold_header"><span class="pink_highlight2 "><?php echo 'PHP ' . $value['style_price'] * $value['style_quantity']; ?></span></td>
                             </tr>
                             <?php } ?>
 
@@ -156,10 +96,10 @@
                     <!-- cart info -->
                     <div class="row mt-3 p-0 mb-0 cart_info">
                         <div class="col-6">
-                            <p>Order Date</p>
+                            <p>Order ID</p>
                         </div>
                         <div class="col-6 text-end">
-                            <p><?php echo date('Y-m-d H:i:s'); ?></p>
+                            <p>12345</p>
                         </div>
                     </div>
                     <div class="row p-0 mb-0 cart_info">
@@ -170,40 +110,34 @@
                             <p><?php echo $_SESSION['total_items']?></p>
                         </div>
                     </div>
-                    <div class="row p-0 mb-0 cart_info">
-                        <div class="col-6">
-                            <p class="bold_header">SHIPPING FEE</p>
-                        </div>
-                        <div class="col-6 text-end">
-                            <p class="bold_header">100</p>
-                        </div>
-                    </div>
                     <div class="row p-0 mb-2 cart_info border-bottom">
                         <div class="col-6">
                             <p class="bold_header">TOTAL</p>
                         </div>
                         <div class="col-6 text-end">
-                            <p class="pink_highlight bold_header"><?php echo 'PHP ' . $_SESSION['grand_total']?></p>
+                            <p class="pink_highlight bold_header"><?php echo 'PHP ' . $_SESSION['total']?></p>
                         </div>
                     </div>
-                    <div class="row pt-2 m-0">
+                    <!-- <div class="row pt-2 m-0">
                         <div class="payment_details filter_content px-0">
-                            <form method = "POST" action = "checkout_page.php">
                             <h6 class="bold_header">Select your Payment Method</h6>
                             <div class="form-check ms-3">
-                                <input class="form-check-input" name="payment_method" type="radio" value="Cash on Delivery" id="Cash">
-                                <label class="form-check-label ms-1" for="small">Cash on Delivery</label>
+                                <input class="form-check-input" name="payment_method" type="radio" value="" id="Cash">
+                                <label class="form-check-label ms-1" for="small">Cash</label>
                             </div>
                             <div class="form-check ms-3">
-                                <input class="form-check-input" name="payment_method" type="radio" value="GCash" id="GCash">
+                                <input class="form-check-input" name="payment_method" type="radio" value="" id="Credit Card">
+                                <label class="form-check-label ms-1" for="small">Credit Card</label>
+                            </div>
+                            <div class="form-check ms-3">
+                                <input class="form-check-input" name="payment_method" type="radio" value="" id="GCash">
                                 <label class="form-check-label ms-1" for="small">GCash</label>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                     <div class="pink_btn2 mt-3 pb-2">
-                        <button class="btn btn-dark border-0 rounded-1 w-100" type="submit" name="place_order" >PLACE ORDER</button>
+                        <a href="#"><button class="btn btn-dark border-0 rounded-1 w-100">PLACE ORDER</button></a>
                     </div>
-                    </form>
                 </div>
             </div>
         </div>
