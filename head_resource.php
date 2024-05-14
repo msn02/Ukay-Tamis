47b11a8869158bf40112616d63daed43ba0b47f2
Guirald Escober <guiraldmaloneespaldon.escober@bicol-u.edu.ph>
2024-05-14 16:19:39 +0800

    Account Page 2/4 Functions
    feat(acc): add password and security question functions using ajax and php. Added general modals for error and successful function invoking

--- head_resource.php
+++ head_resource.php
@@ -9,6 +9,8 @@
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
 <script src="javascript/main_script.js" defer type="text/javascript"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
 
 <!--<script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script>-->
 <style>
