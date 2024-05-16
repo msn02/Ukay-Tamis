6796ef1f16855d03d8b6e993a8a0a945856424b7
Daniela Cantillo <danielamcantillo@gmail.com>
2024-05-16 16:14:50 +0800

    refactor: fix query on mystery box review

--- server/get_mystery_box_review.php
+++ server/get_mystery_box_review.php
@@ -6,7 +6,8 @@
     $stmt = $conn -> prepare ("SELECT reviews.*, user.* 
                                 FROM reviews 
                                 INNER JOIN user ON reviews.user_id = user.user_id 
-                                WHERE reviews.style_id = 'style-0068' LIMIT 3");
+                                WHERE reviews.style_id = 'style-0068' 
+                                ORDER BY reviews.review_id DESC LIMIT 3");
 
     // Execute the query
     $stmt -> execute();
